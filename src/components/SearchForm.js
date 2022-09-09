557da79f0908e9a5c500816c9350f93b8a492afa
Sushant Prasad <sushantprasad1234@gmail.com>
2022-09-09 19:08:14 +0530

    commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search here</label>
+          <input
+            type="text"
+            ref={searchValue}
+            id="name"
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, est
+        cumque exercitationem fugit eum quae omnis ipsa nemo esse iure
+        voluptates sint excepturi rerum. Aperiam minus harum architecto nemo.
+        Excepturi sapiente omnis hic, nisi cum, molestiae saepe est eligendi
+        voluptates veniam quisquam consequatur deserunt sequi eaque dolore
+        incidunt laborum adipisci.
+      </p>
+    </section>
+  );
+};
+
+export default About;
